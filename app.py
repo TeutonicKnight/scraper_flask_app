cd8545578cd6b94dd136cfcc78577549aaec42e4
TeutonicKnight <kyberkittie@protonmail.com>
2020-10-10 15:40:00 +0200

    committing aoe2 fandom scraper functionality

--- app.py
+++ app.py
@@ -1,19 +1,59 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
+import requests
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Result
+from models import *
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	errors = []
+	results = {}
+	if request.method == "POST":
+		# get url that the person has entered
+		try:
+			url = request.form['url']
+			r = requests.get(url).text
+		except:
+			errors.append(
+				"Unable to get URL. Please make sure it's valid and try again."
+			)
+			return render_template('index.html', errors=errors)
+		if r:
+			# scraping
+			raw = BeautifulSoup(r, 'lxml')
+			table = raw.find('table', attrs={'class': 'wikitable'})
+			table_rows = table.find_all('tr')
+			dic = {}
+			for tr in table_rows[1:-1]:
+				td = tr.find_all('td')
+				row = [i.text for i in td]
+				dic[row[0].strip()] = row[3].strip()
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+			# save the results to be rendered in html
+			results = dic.items()
+
+			# add results to database
+			try:
+				for key, value in dic.items():
+				    result = Result(
+				        siege_weapon=key,
+				        HP=f'{value}',
+				    )
+				    db.session.add(result)
+				db.session.commit()
+			except Exception as e:
+				errors.append(e)
+				errors.append("Unable to add item to database.")
+	return render_template('index.html', errors=errors, results=results)
+
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
 
--- models.py
+++ models.py
@@ -1,19 +1,16 @@
 from app import db
-from sqlalchemy.dialects.postgresql import JSON
 
 
 class Result(db.Model):
     __tablename__ = 'results'
 
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String())
-    result_all = db.Column(JSON)
-    result_no_stop_words = db.Column(JSON)
+    siege_weapon = db.Column(db.String())
+    HP = db.Column(db.String())
 
-    def __init__(self, url, result_all, result_no_stop_words):
-        self.url = url
-        self.result_all = result_all
-        self.result_no_stop_words = result_no_stop_words
+    def __init__(self, siege_weapon, HP):
+        self.siege_weapon = siege_weapon
+        self.HP = HP
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
